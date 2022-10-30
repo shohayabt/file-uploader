1d321b69d8ac49021d94b07506decc7e63062fce
shohayabt <shohayabhasan@yahoo.com>
2022-10-30 07:28:33 +0600

    errorHandling is working with file uploading

--- serverside-code/index.js
+++ serverside-code/index.js
@@ -10,16 +10,41 @@ app.use(express.json())
 const UPLOAD_FOLDER =  './uploads/';
 // multer workstation
 const upload = multer({ // it will return a middleware 
-    dest: UPLOAD_FOLDER
+    dest: UPLOAD_FOLDER,
+    limits:{
+        fileSize:300000, // 300KB
+    },
+    fileFilter:(req,file,cb)=>{
+        if(
+            file.mimetype === "image/png"
+        ){
+            cb(null,true)
+        }else{
+            cb('Only png file is allowed')
+        }
+    }
 })
 
 
 app.get("/",(req,res)=>{
     res.send("Hello Programmer")
 })
-app.post("/upload", upload.fields([{name:'profilePicture', maxCount : 1 },{name:'coverPhoto', maxCount : 1 }]
-    ), (req,res)=>{
+app.post("/upload", upload.single("profilePicture"), (req,res)=>{
     res.json({"status":"successfull"})
+})
+
+
+// Error Handler 
+app.use((err ,req,res,next)=>{
+    if(err){
+        if(err instanceof multer.MulterError){
+            res.status(500).send(err.message)
+        }else{
+            res.status(500).send("something went wrong")
+        }
+    }else{
+        res.status(200).json({"status":"successfull"})
+    }
 })
 app.listen(port,()=>{
     console.log(`SERVER IS UP - PORT:${port}`)
