1448537d6746b35814fb14f9d0e4168f4ae1245c
shohayabt <shohayabhasan@yahoo.com>
2022-10-30 06:16:08 +0600

    file is uploading successfully

--- serverside-code/index.js
+++ serverside-code/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const multer = require("multer")
 const app = express();
 const port = 3000 || 5000
 
 
+app.use(express.json())
 
-app.get("/", (req,res)=>{
-    res.send("Hello Programmers")
+// LocalFileSystem 
+const UPLOAD_FOLDER =  './uploads/';
+// multer workstation
+const upload = multer({ // it will return a middleware 
+    dest: UPLOAD_FOLDER
+})
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello Programmer")
+})
+app.post("/upload", upload.single('profilePicture'), (req,res)=>{
+    res.json({"status":"successfull"})
 })
 app.listen(port,()=>{
     console.log(`SERVER IS UP - PORT:${port}`)
